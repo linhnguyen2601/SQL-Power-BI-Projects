b181e88eca6255a19f50704bbe45892c6d1c884a
linhnguyen2601 <166676829+linhnguyen2601@users.noreply.github.com>
2024-06-23 23:34:56 +0700

    Create Coffee Brand Review.sql

---
+++ Coffee Brand Review.sql
@@ -0,0 +1,13 @@
+CREATE TABLE CoffeeBrandReview
+(
+	Brand_name varchar(50),
+	Roasters varchar(25),
+	Type_of_roast varchar(25),
+	Roaster_Location varchar(25),
+	Price_per_100g float,
+	Rating float,
+	Review_date date,
+	Short_product_description varchar(50)
+	)
+select * from CoffeeBrandReview
+
