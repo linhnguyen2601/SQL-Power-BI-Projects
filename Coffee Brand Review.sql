b9b1b84a19adaa95f055402c486869e3f3151006
linhnguyen2601 <166676829+linhnguyen2601@users.noreply.github.com>
2024-06-24 00:13:21 +0700

    Update Coffee Brand Review.sql

--- Coffee Brand Review.sql
+++ Coffee Brand Review.sql
@@ -4,7 +4,7 @@ CREATE TABLE CoffeeBrandReview
 	Roasters varchar(40),
 	Type_of_roast varchar(25),
 	Roaster_Location varchar(25),
-	Origin varchar (25),
+	Origin varchar (50),
 	Price_per_100g float,
 	Rating float,
 	Review_date date,
@@ -12,5 +12,20 @@ CREATE TABLE CoffeeBrandReview
 	)
 select * from CoffeeBrandReview
 
-ALTER TABLE CoffeeBrandReview
-ADD COLUMN Origin varchar(25)
+
+Copy CoffeeBrandReview from 'C:\Program Files\PostgreSQL\15\data\Coffee Brand Review.csv' csv header
+
+select 
+count(distinct(roasters)),
+count(distinct(type_of_roast)),
+count(distinct(roaster_location)),
+count(distinct(origin)), 
+count(distinct(short_product_description))
+from coffeebrandreview
+=> 294, 5, 11, 27, 999
+
+-- Nhung brand name dc danh gia nhieu hon 1 lan	
+select brand_name, 
+row_number() over(partition by brand_name)
+from coffeebrandreview
+order by row_number() over(partition by brand_name) desc
