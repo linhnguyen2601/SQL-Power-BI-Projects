5c7aa060e8e85141bb43a478f05c17589d75f4bf
linhnguyen2601 <166676829+linhnguyen2601@users.noreply.github.com>
2024-06-23 23:57:58 +0700

    Update Coffee Brand Review.sql

--- Coffee Brand Review.sql
+++ Coffee Brand Review.sql
@@ -1,13 +1,16 @@
 CREATE TABLE CoffeeBrandReview
 (
-	Brand_name varchar(50),
-	Roasters varchar(25),
+	Brand_name varchar(100),
+	Roasters varchar(40),
 	Type_of_roast varchar(25),
 	Roaster_Location varchar(25),
+	Origin varchar (25),
 	Price_per_100g float,
 	Rating float,
 	Review_date date,
-	Short_product_description varchar(50)
+	Short_product_description varchar(150)
 	)
 select * from CoffeeBrandReview
 
+ALTER TABLE CoffeeBrandReview
+ADD COLUMN Origin varchar(25)
